724202e9a9a3c1b2c40dc8220ca2fa0a1af58a9b
David Edwards <david.edwards@cellularsales.com>
2013-11-06 10:17:29 -0500

    Clean up project.  Add BLL Model Project

--- KCD/KCD/Controllers/HomeController.cs
+++ KCD/KCD/Controllers/HomeController.cs
@@ -13,17 +13,15 @@ namespace KCD.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        public ActionResult CreateAnAccount()
+        {
             return View();
         }
     }
---
+++ KCD/Shared/Security/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Security
+{
+    public class Account
+    {
+        #region Properties
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string Company { get; set; }
+        public bool CanSendEmails { get; set; }
+        public bool CanBeContactedBySponsers { get; set; }
+        public string Password { get; set; }
+        #endregion
+    }
+}
