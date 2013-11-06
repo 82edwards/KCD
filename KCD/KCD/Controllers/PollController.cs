6f323df09738c1dca12997df3586561797c0f47e
David Edwards <david.edwards@cellularsales.com>
2013-11-06 16:04:28 -0500

    initial setup of poll controllers

---
+++ KCD/KCD/Controllers/PollController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KCD.Controllers
+{
+    public class PollController : Controller
+    {
+        //
+        // GET: /Poll/
+        public ActionResult CreateAPoll()
+        {
+            return View();
+        }
+	}
+}
