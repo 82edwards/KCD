b7d6b1d834475c42cc06f834721da7b39cc71bb2
David Edwards <david.edwards@cellularsales.com>
2013-12-19 16:39:01 -0500

    Add header files to each of the sections

---
+++ KCD/KCD/Controllers/EventController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace KCD.Controllers
+{
+    public class EventController : Controller
+    {
+        public ActionResult EventList()
+        {
+            return View();
+        }
+    }
+}
