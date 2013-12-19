a9ad33b65430afd81e458b616ec93fa483ffb57a
David Edwards <david.edwards@cellularsales.com>
2013-12-19 17:08:25 -0500

    Add ability to display multiple polls.

---
+++ KCD/KCD/ViewModel/ViewPolls.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KcdModel.Poll;
+
+namespace KCD.ViewModel
+{
+    public class ViewPolls
+    {
+        #region Properties
+        public List<Poll> Polls { get; set; } 
+        #endregion
+
+        public ViewPolls(int pollsStatus)
+        {
+            Polls = Poll.GetPolls(pollsStatus);
+        }
+    }
+}
--- KCD/Shared/Poll/Poll.cs
+++ KCD/Shared/Poll/Poll.cs
@@ -1,4 +1,7 @@
 ﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using KcdModel.Helper;
 
 namespace KcdModel.Poll
 {
@@ -13,7 +16,43 @@ namespace KcdModel.Poll
 
         public void Create()
         {
-            throw new System.NotImplementedException();
+            using (var conn = Sql.GetSqlConnection())
+            {
+                using (var cmd = new SqlCommand
+                {
+                    CommandType = CommandType.StoredProcedure,
+                    CommandText = "",
+                    Connection = conn,
+                    Parameters = { new SqlParameter() }
+                })
+                {
+                    conn.Open();
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static List<Poll> GetPolls(int pollsStatus)
+        {
+            using (var conn = Sql.GetSqlConnection())
+            {
+                conn.Open();
+                using (var dr = (new SqlCommand
+                {
+                    Connection = conn,
+                    CommandText = "dbo.GetPollsByStatus",
+                    CommandType = CommandType.StoredProcedure,
+                    Parameters = { new SqlParameter("@StatusId", pollsStatus) }
+                }).ExecuteReader())
+                {
+                    var results = new List<Poll>();
+                    while (dr.Read())
+                    {
+                        results.Add(new Poll());
+                    }
+                    return results;
+                }
+            }
         }
     }
 }
